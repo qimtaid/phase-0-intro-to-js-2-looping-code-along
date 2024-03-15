f472e9f293025fcad8ca85c648b125f01f054c80
Dan Koskei <qimtaid@gmail.com>
2024-03-15 16:23:11 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+// index.js
+
+// Function to generate thank you messages
+function writeCards(names, event) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+}
+
+// Function to count down from a given number
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+// Exporting the functions if needed in a Node.js environment
+module.exports = {
+    writeCards,
+    countDown
+};
